b1ce367259be11eb4be5bbd8f46c34ec31b879c6
louna <r.arswendo.r@gmail.com>
2024-07-05 13:40:45 +0700

    feat: 3D tube

--- src/internal/shapes/circle.ts
+++ src/internal/shapes/circle.ts
@@ -1,6 +1,7 @@
 export class Position {
     x: number
     y: number
+
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
@@ -8,9 +9,23 @@ export class Position {
 }
 
 export function drawCircle(element: HTMLCanvasElement, position: Position, radius: number, color: string) {
-    const ctx = element.getContext("2d");
+    const ctx = element.getContext("2d")!;
     ctx.beginPath();
     ctx.arc(position.x, position.y, radius, 0, 2 * Math.PI);
     ctx.fillStyle = color;
     ctx.fill();
+}
+
+export function draw3DTube(ctx: CanvasRenderingContext2D, element: HTMLCanvasElement, position: Position, radius: number) {
+    drawCircle(element, position, 30, "red");
+    const cx = position.x;
+    const yx = position.y;
+    for (let d = 0; d < 360 + 1; d++) {
+        const x = cx + (radius * Math.cos(d * Math.PI / 180))
+        const y = yx + (radius * Math.sin(d * Math.PI / 180))
+        const scalar = 2;
+        ctx.moveTo(x, y);
+        ctx.lineTo((scalar * x) - (element.width / 2), (scalar * y) - (element.height / 2));
+        ctx.stroke();
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import {drawCircle, Position} from "./internal/shapes/circle.ts";
+import {draw3DTube, Position} from "./internal/shapes/circle.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div>
@@ -9,28 +7,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
 `
 
-const canvas = document.querySelector<HTMLCanvasElement>("#CircleCanvas");
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector<HTMLCanvasElement>("#CircleCanvas")!;
+const ctx = canvas.getContext('2d')!;
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-let mousePosition = null;
+let mousePosition: Position | null = null;
 
 async function animate() {
-    const canvas = document.querySelector<HTMLCanvasElement>("#CircleCanvas");
+    const canvas = document.querySelector<HTMLCanvasElement>("#CircleCanvas")!;
     canvas.addEventListener("mousemove", (mouseEvent) => {
         mousePosition = new Position(mouseEvent.x, mouseEvent.y);
     })
     while (true) {
         if (mousePosition) {
-            drawCircle(canvas, mousePosition, 30, "black");
-            const cx = mousePosition.x;
-            const yx = mousePosition.y;
-            for (let d = 0; d < 360 + 1; d++) {
-                const x = cx + (30 * Math.cos(d * Math.PI / 180))
-                const y = yx + (30 * Math.sin(d * Math.PI / 180))
-                drawCircle(canvas, new Position(x, y), 1, 'red');
-            }
+            // ctx.fillText(`Coordinate: ${mousePosition.x - (canvas.width / 2)}, ${mousePosition.y - (canvas.height / 2)}`, canvas.width/8, canvas.height/9);
+            const centered = new Position(mousePosition.x - (canvas.width / 2), mousePosition.y - (canvas.height / 2));
+            ctx.fillText(`Coordinate: ${mousePosition.x}, ${mousePosition.y}`, canvas.width / 8, canvas.height / 9);
+            ctx.fillText(`Centered Coordinate: ${centered.x}, ${centered.y}`, canvas.width / 8, canvas.height / 7.5);
+            // canvas.width
+            draw3DTube(ctx, canvas, mousePosition, 30)
         }
 
         await sleep(1);
